69c02737196297bf5b6209e1d169f3539d59faf1
abbygaylepandaan <abbypandaan16@gmail.com>
2025-01-29 23:09:59 +0800

    added ga4

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-6YZHZYSQL4');
+
+ReactGA.send({ hitType: 'pageview', page: '/my-path', title: 'Custom Title' });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
